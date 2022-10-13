a28eb303afdceeb4d01d1976a4b451d1002a2849
MateusFCD <mateusdrumondicm@gmail.com>
2022-10-13 16:05:20 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+import { Router } from "./Router";
+import { BrowserRouter } from "react-router-dom";
+
 export function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyle />
+        <Router />
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
- 
