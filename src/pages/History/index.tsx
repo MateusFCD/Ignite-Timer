3c8e98e234008500dc4f27f2483c807eb0e9c6fa
Mateus Drumond <mateusdrumondicm@gmail.com>
2022-10-17 21:28:38 -0300

    update

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,9 +1,9 @@
-import { HistoryContainer, HistoryList } from "./styles";
+import { HistoryContainer, HistoryList, Status } from "./styles";
 
 export function History() {
   return (
     <HistoryContainer>
-      <h1>historico</h1>
+      <h1>Meu hist</h1>
 
       <HistoryList>
         <table>
@@ -20,19 +20,25 @@ export function History() {
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td>Status</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td>Status</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td>Status</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
             </tr>
           </tbody>
         </table>
