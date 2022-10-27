a23220b54318f78370a993f4c74591a028266805
MateusFCD <mateusdrumondicm@gmail.com>
2022-10-27 12:22:00 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 import { Router } from "./Router";
 import { BrowserRouter } from "react-router-dom";
+import { CyclesContextProvider } from "./contexts/CyclesContext";
 
 export function App() {
   return (
     <BrowserRouter>
       <ThemeProvider theme={defaultTheme}>
         <GlobalStyle />
-        <Router />
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
       </ThemeProvider>
     </BrowserRouter>
   );
